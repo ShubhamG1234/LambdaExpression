eef117cb530d6fb9b73561bbec305de03db830f8
ShubhamG1234 <100414848+ShubhamG1234@users.noreply.github.com>
2022-08-11 21:37:08 +0530

    epression lambda
    lambda epression example

---
+++ LamdaExpression/LamdaExpression/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaExpression
+{
+    public class Person
+    {
+        public string SSN;
+        public string Name;
+        public int Age;
+        public string Address;
+
+        public Person(string sSN, string name, string address, int age)
+        {
+            SSN = sSN;
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+    }
+}
